a96712d513e83d6f90ba1986300b6056a4f70302
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-07-21 08:27:13 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 # Installs usefull programs
-chmod +x perm.sh
 
 echo "Running Update"
 sudo ./update.sh
 
 echo "Installing Git & Fortune"
 sudo apt install git fortune
+
+sudo usermod -aG sudo $USER
