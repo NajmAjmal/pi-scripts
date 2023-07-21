9fbe7c2043d392c6c700f67c525c8b84317b454e
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-07-21 05:14:36 -0700

    Added Pi-apps perms

--- perm.sh
+++ perm.sh
@@ -1,6 +1,15 @@
 # Gives Permissions to all programs in the repo
 
+# install directory perms
 chmod +x server.sh
+
+# all directory perms
 cd all
 chmod +x update.sh
 chmod +x setup.sh
+cd -
+
+## GUI directory perms
+cd GUI
+chmod +x pi-apps.sh
+cd -
