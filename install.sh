ddf224d72349fe7330d2705987d5084f9708dc8c
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-07-21 08:48:03 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
+# Going to main directory and removing existing pi-scripts, used for updating
+cd
 rm -r -f pi-scripts
+# Installing pi-scripts
 git clone https://github.com/NajmAjmal/pi-scripts
 cd pi-scripts
+# Giving perms to all scripts
 chmod +x perm.sh
 ./perm.sh
