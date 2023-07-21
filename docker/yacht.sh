5cdd435583848d0e080aa7c101d595874d7dee3f
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-07-21 08:25:26 -0700

    Update yacht.sh

--- docker/yacht.sh
+++ docker/yacht.sh
@@ -1,3 +1,3 @@
-docker volume create yacht && docker run -d --restart unless-stopped --name yacht -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -v yacht:/config selfhostedpro/yacht
+sudo docker volume create yacht && docker run -d --restart unless-stopped --name yacht -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -v yacht:/config selfhostedpro/yacht
 echo "Yacht has been installed"
 echo "View WebUI: localhost:8000"
