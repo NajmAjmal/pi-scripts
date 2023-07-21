8026c9c6fa2ac667417f996248139a71c2ce76af
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-07-21 07:19:34 -0700

    Create htop.sh

---
+++ all/htop.sh
@@ -0,0 +1,7 @@
+echo "Installing htop"
+echo "An interactive process viewer and system monitor. It provides a better and more user-friendly alternative to the standard top command."
+
+./update.sh
+sudo apt install htop
+
+echo "To run use the htop command"
