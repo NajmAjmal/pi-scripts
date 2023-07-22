f5beeb59247a003432b7ac6fb26dbc1903b4ad48
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-07-22 00:26:36 -0700

    Update yacht.sh

--- docker/yacht.sh
+++ docker/yacht.sh
@@ -3,3 +3,9 @@ sudo docker run -d --restart always --name yacht -p 8000:8000 -v /var/run/docker
 
 echo "Yacht has been installed"
 echo "View WebUI: localhost:8000"
+
+echo ""
+
+echo "Add these templates:"
+echo ""
+echo "https://yangkghjh.github.io/selfhosted_store/unraid/templates/yacht/yacht.json"
