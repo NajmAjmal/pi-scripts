ba0ff301f014bb9d04571c9e84459e5b39ec639d
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-07-22 13:14:45 -0700

    Update docker-install.sh

--- docker/docker-install.sh
+++ docker/docker-install.sh
@@ -7,9 +7,5 @@ curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker $USER
 echo "Docker Installed"
 echo ""
-echo "Installing Docker Compose"
-sudo apt-get install -y libffi-dev libssl-dev python3 python3-pip
-sudo pip3 install docker-compose
-echo "Docker Compose Installed"
 
 sudo docker run hello-world
