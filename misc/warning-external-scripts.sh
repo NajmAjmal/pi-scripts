dc26bba9935b84b821fbcbe2a640d9123dd5c6a1
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-07-22 13:00:36 -0700

    Create warning-external-scripts.sh

---
+++ misc/warning-external-scripts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Function to display prompt and read user input
+function ask_yes_no {
+    while true; do
+        read -p "$1 (y/n): " answer
+        case "$answer" in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer 'yes' or 'no'.";;
+        esac
+    done
+}
+
+# Main script
+echo "=======  WARNING ========"
+echo "The script you are about to run is an external script."
+echo ""
+
+# Prompt the user to continue with the warning
+if ! ask_yes_no "Do you want to continue?"; then
+    echo "You chose 'no'. Script terminated."
+    exit 1
+fi
+
+# The rest of your script goes here if the user chose 'yes'
+echo "Script execution completed."
