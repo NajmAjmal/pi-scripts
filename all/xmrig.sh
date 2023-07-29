6f4d94f605f546fd5c2f2218d75a361bc1720c5f
Najm Ajmal <najmajmal@mail.com>
2023-07-29 01:04:15 -0700

    Update xmrig.sh

--- all/xmrig.sh
+++ all/xmrig.sh
@@ -1,38 +1,8 @@
 #!/bin/bash
 
-# Function to display prompt and read user input
-function ask_yes_no {
-    while true; do
-        read -p "$1 (y/n): " answer
-        case "$answer" in
-            [Yy]* ) return 0;;
-            [Nn]* ) return 1;;
-            * ) echo "Please answer 'yes' or 'no'.";;
-        esac
-    done
-}
-
-xmrig_automine() {
-  # Get the current user
-  user=$(whoami)
-
-  # Command to be added to crontab
-  cron_command="@reboot sudo ./$home/$user/mine.sh"
-
-  # Add the command to the user's crontab
-  echo "$cron_command" >> "/var/spool/cron/crontabs/$user"
-
 echo "Installing XMRig"
 
 ./update.sh
-
-# Ask if the user wants to add an additional feature
-if ask_yes_no "Do you want xmrig to auto-mine at boot"; then
-    # Add the extra command here that should be executed if the user chose 'yes'
-    xmrig_automine
-else
-    
-fi
 
 echo "Installing Dependency"
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
