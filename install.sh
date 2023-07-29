3fc31fb79c2ef66878f555b554146d22e8a8853c
Najm Ajmal <najmajmal@mail.com>
2023-07-29 00:42:08 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
-echo "Installing Git"
-sudo apt install git -y
+echo "Giving sudo perms"
+sudo usermod -aG sudo $USER
+
+echo "Installing Git & Fortune"
+sudo apt install git fortune -y
+
+
 # Going to main directory and removing existing pi-scripts, used for updating
 cd
 sudo rm -r -f pi-scripts
@@ -9,5 +14,5 @@ cd pi-scripts
 # Giving perms to all scripts
 sudo chmod +x perm.sh
 ./perm.sh
-# Setup
-./setup.sh
+
+sudo ./all/update.sh
