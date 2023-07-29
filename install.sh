39aaf783b42f3b67dcda1c26eeab74547b39703e
Najm Ajmal <najmajmal@mail.com>
2023-07-29 00:37:02 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
+echo "Installing Git"
+sudo apt install git -y
 # Going to main directory and removing existing pi-scripts, used for updating
 cd /$home/$user/
-rm -r -f pi-scripts
+sudo rm -r -f pi-scripts
 # Installing pi-scripts
 git clone https://github.com/NajmAjmal/pi-scripts
 cd pi-scripts
 # Giving perms to all scripts
-chmod +x perm.sh
+sudo chmod +x perm.sh
 ./perm.sh
 # Setup
 ./setup.sh
