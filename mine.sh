5d59c0c216aceed75cf99bffd4135014f24bfc9c
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-08-19 01:09:18 -0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,2 @@
-./all/xmrig -o gulf.moneroocean.stream:10128 -u 4657q4dnsjLWtzeW4XN3wG9swFumWAZB9i1pegTLMxVAQy5E5AE8uif42kkHWcWc9vDcLUmzeCf3pV7mmrJQQqqe84dtASi -p pi
+cd /
+./xmrig/build/xmrig -o gulf.moneroocean.stream:10128 -u 4657q4dnsjLWtzeW4XN3wG9swFumWAZB9i1pegTLMxVAQy5E5AE8uif42kkHWcWc9vDcLUmzeCf3pV7mmrJQQqqe84dtASi -p pi
